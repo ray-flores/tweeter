b90469f7dc2eec0c719d7c131b571bbb8d96c891
Ray Flores <ray.m.flores@gmail.com>
2021-12-16 01:29:51 +0000

    completed text counter feature

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,30 @@
+
+
+
+$(document).ready(function() { //call jQuery with obj from request
+  
+
+  $('#tweet-text').on('keyup', function() {
+    let tweet = $(this).val().length;
+    let limit = 140 - tweet;
+    if (limit < 0) {
+      $('.counter').addClass('negative-numbers');
+      $('.counter').css('color', 'red');
+
+    }
+    if (limit >= 0) {
+      $('.counter').removeClass('negative-numbers');
+      $('.counter').css('color', '#545149');
+    }
+    
+    $('.counter').text(limit);
+
+
+  });
+
+  
+
+
+
+  
+});
